5dac2466cc5d130ceeafec0548acd729c7c29aae
Doc McCoy <renan_ota@hotmail.com>
2018-08-17 00:35:00 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import requests, random, pprint
+
+def generatePeople(quantidade):
+    """ Busca pessoas no 4devs e retorna um dict com elas. """
+
+    gerados = []
+
+    for i in range(quantidade):
+
+        idade   = random.randint(18, 90)
+        sexo    = random.choice(['H', 'M'])
+
+        pessoa = requests.post('https://www.4devs.com.br/ferramentas_online.php', data = {
+            'acao'      : 'gerar_pessoa',
+            'idade'     : idade,
+            'pontuacao' : 'S',
+            'sexo'      : sexo
+        })
+
+        gerados.append(pessoa.json())
+
+    return gerados
+
+def saveData(data):
+    """ Insere os dados no banco de dados. """
+    pass
+
+def main():
+
+    print("Bem vindo ao populador de banco de dados!")
+    quantidade = int(input("Quantas pessoas pretende gerar?\n"))
+    gerados = generatePeople(quantidade)
+
+if __name__ == '__main__':
+    main()
