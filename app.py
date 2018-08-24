1ccf01003633bbe02ada8e24b154d202b5177600
Renan Santana <renan_ota@hotmail.com>
2018-08-24 18:21:57 -0300

    Transformei tudo em uma classe

--- app.py
+++ app.py
@@ -1,81 +1,92 @@
+#!python3
+# Autor: Renan Santana Desiderio
 import requests, random, pymysql.cursors, configparser
+from tkinter import *
 
-def getConfig():
-    """ Pega os dados do config """
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config['db']
+class App:
 
-def generatePeople(quantidade):
-    """ Busca pessoas no 4devs e retorna um dict com elas. """
+    def __init__(self):
+        """ Cria a conexão com o banco de dados """
 
-    gerados = []
+        db = self.getConfig()
 
-    for i in range(quantidade):
+        self.conexao = pymysql.connect(
+            host = db['host'],
+            user = db['user'],
+            password = db['password'],
+            db = db['db'],
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        self.cursor = self.conexao.cursor()
 
-        idade   = random.randint(18, 90)
-        sexo    = random.choice(['H', 'M'])
+    def setTable(self, table):
+        """ Seta uma tabela para ser populada. """
+        
+        # Verificar se a tabela passada existe
+        sql = "SHOW TABLES LIKE '{}'".format(table)
+        self.cursor.execute(sql)
+        result = self.cursor.fetchone()
+        if len(result != 0):
+            self.table = table
+        else:
+            print("A tabela digitada não existe no banco de dados.")
+            return False
 
-        pessoa = requests.post('https://www.4devs.com.br/ferramentas_online.php', data = {
-            'acao'      : 'gerar_pessoa',
-            'idade'     : idade,
-            'pontuacao' : 'S',
-            'sexo'      : sexo
-        })
+    def getConfig(self):
+        """ Pega os dados do config """
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        return config['db']
 
-        gerados.append(pessoa.json())
+    def generatePeople(self, quantidade):
+        """ Busca pessoas no 4devs e retorna um dict com elas. """
 
-    return gerados
+        gerados = []
 
-def saveData(table, data):
-    """ Insere os dados no banco de dados. """
+        for i in range(quantidade):
 
-    db = getConfig()
+            idade   = random.randint(18, 90)
+            sexo    = random.choice(['H', 'M'])
 
-    conexao = pymysql.connect(
-        host = db['host'],
-        user = db['user'],
-        password = db['password'],
-        db = db['db'],
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    cursor = conexao.cursor()
-    sql = "INSERT INTO {} (nome) VALUES (%s)".format(table)
-    cursor.execute(sql, (data[0]['nome']))
-    conexao.commit()
-    conexao.close()
+            pessoa = requests.post('https://www.4devs.com.br/ferramentas_online.php', data = {
+                'acao'      : 'gerar_pessoa',
+                'idade'     : idade,
+                'pontuacao' : 'S',
+                'sexo'      : sexo
+            })
 
-def getColumnsOfTable(table):
-    """ Retorna um array com os nomes das colunas de uma tabela. """
+            gerados.append(pessoa.json())
 
-    db = getConfig()
+        return gerados
 
-    conexao = pymysql.connect(
-        host = db['host'],
-        user = db['user'],
-        password = db['password'],
-        db = db['db'],
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    cursor = conexao.cursor()
+    def saveData(self, data):
+        """ Insere os dados no banco de dados. """
 
-    sql = "SHOW COLUMNS FROM {}".format(table)
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    colunas = [column.get('Field') for column in result]
-    return colunas
+        sql = "INSERT INTO {} (nome) VALUES (%s)".format(self.table)
+        self.cursor.execute(sql, (data[0]['nome']))
+        self.conexao.commit()
+        # self.conexao.close()
+
+    def getColumnsOfTable(self):
+        """ Retorna um array com os nomes das colunas de uma tabela. """
+
+        sql = "SHOW COLUMNS FROM {}".format(self.table)
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        colunas = [column.get('Field') for column in result]
+        return colunas
 
 def main():
+
+    app = App()
 
     print("Bem vindo ao populador de banco de dados!")
 
     tabela = input("Qual tabela deseja popular?\n")
-    colunas = getColumnsOfTable(tabela)
+    app.setTable(tabela)
+    colunas = app.getColumnsOfTable()
     print("Colunas da tabela:")
     [print(coluna) for coluna in colunas]
-    campos = input("Digite as colunas que deseja popular, separadas por virgula:\n")
-    campos = campos.replace(" ", "")
-    colunasSelecionadas = campos.split(",")
 
     # quantidade  = int(input("Quantas pessoas pretende gerar?\n"))
     # gerados     = generatePeople(quantidade)
